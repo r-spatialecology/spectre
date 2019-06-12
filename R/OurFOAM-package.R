73fb9b6ae1f50cac14d402ce73cff0c0fa918955
marco <sciaini.marco@gmail.com>
2019-06-12 09:11:31 +0200

    :sparkle: tidy up bits and pieces

--- R/OurFOAM-package.R
+++
@@ -1,6 +0,0 @@
-## usethis namespace: start
-#' @importFrom Rcpp sourceCpp
-## usethis namespace: end
-NULL
-
-globalVariables(c("PlotID", "alpha_list", "bmo", "cellid", "lat", "lon", "lut", "plotID", "x", "y"))
---
+++ R/spectre-package.R
@@ -0,0 +1,15 @@
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+## usethis namespace: end
+NULL
+
+globalVariables(c("PlotID", 
+                  "alpha_list",
+                  "bmo", 
+                  "cellid", 
+                  "lat",
+                  "lon", 
+                  "lut", 
+                  "plotID",
+                  "x",
+                  "y"))
