e072adffd3fd8b8e736488ea3b34f15df8539e22
cloud <cloud@rstudio-sebastian.novalocal>
2019-07-12 15:12:00 +0200

    :racehorse: Faster calculate_solution_commoness*
    - calculate_solution_commoness_rcpp now calculates the lower left triangular matrix, only -> 2x faster
    
    - calculate_solution_commoness_rcpp_p is the parallel version. On a slow 32-core machine and medium-sized matrix, it was 50x faster
    
    - calculate_solution_commoness_site_rcpp_p is a parallel version of calculate_solution_commoness_site_rcpp. On a slow 32-core machine and medium-sized matrix, it was 23x faster.

--- src/calculate_solution_commonness.cpp
+++ src/calculate_solution_commonness.cpp
@@ -1,4 +1,5 @@
 #include "calculate_solution_commonness.h"
+#include "omp.h"
 
 // approx 2x faster than old variant
 IntegerMatrix calculate_solution_commonness_rcpp(const IntegerMatrix solution_matrix) {
@@ -30,7 +31,7 @@ IntegerMatrix calculate_solution_commonness_rcpp_p(const IntegerMatrix solution_
 
     // iterate over the lower triangular matrix without diagonal
     // and calculate # of common species between sites
-#pragma omp parallel for
+#pragma omp parallel for schedule(dynamic)
     for (int col = 0; col < nsites - 1; col++) {
         for (int row = col + 1; row < nsites; row++) {
             commonness_mat(row, col) = sum((solution_matrix(_, row) +
@@ -86,27 +87,32 @@ IntegerMatrix calculate_solution_commonness_site_rcpp_p(const IntegerMatrix solu
 
     const int nrows = solution_commonness.nrow();
     const int site_ = site - 1; // because C++ starts indexing at zero
-
-#pragma omp parallel for
-    for (int j = 0; j < site_; j++) {
-        solution_commonness(site_, j) = sum((solution_matrix(_, j) +
-                                             solution_matrix(_, site_)) > 1);
-    }
-
-#pragma omp parallel for
-    for (int j = site_ + 1; j < nrows; j++) {
-        solution_commonness(j, site_) = sum((solution_matrix(_, j) +
-                                             solution_matrix(_, site_)) > 1);
-    }
-
+#pragma omp parallel
+{
+#pragma omp for nowait schedule(static)
+  for (int j = 0; j < site_; j++) {
+    solution_commonness(site_, j) = sum((solution_matrix(_, j) +
+      solution_matrix(_, site_)) > 1);
+  }
+  
+#pragma omp for schedule(static)
+  for (int j = site_ + 1; j < nrows; j++) {
+    solution_commonness(j, site_) = sum((solution_matrix(_, j) +
+      solution_matrix(_, site_)) > 1);
+  }
+}
     return solution_commonness;
 
 }
 
 /*** R
+# Big
 new_solution <- matrix(sample(0:1, 480000, replace = TRUE), 6000, 8000)
-
 solution_commonness <- matrix(sample(0:8, 640000, replace = TRUE), 8000, 8000)
+
+# "Small"
+new_solution <- matrix(sample(0:1, 200000, replace = TRUE), 2000, 3000)
+solution_commonness <- matrix(sample(0:8, 90000, replace = TRUE), 3000, 3000)
 
 random_col <- 68
 
@@ -142,10 +148,20 @@ identical(calculate_solution_commonness_site_R(new_solution, solution_commonness
 # Not identical:
 identical(calculate_solution_commonness_site_R2(new_solution, solution_commonness, random_col), calculate_solution_commonness_site_R(new_solution, solution_commonness, random_col))
 
-bm <- bench::mark(
+bm <- bench::mark(spectre:::calculate_solution_commonness_rcpp_p(new_solution),
+            spectre:::calculate_solution_commonness_rcpp(new_solution),
+            spectre:::calculate_solution_commonness_rcpp_old(new_solution),
+            check = FALSE) # check FALSE is needed because new variant calculates the lower triangular matrix, only
+
+# 1 spectre:::calculate_solution_commonness_rcpp_p(new_solution)   3.07s  3.07s   0.326      34.3MB        0     1     0
+# 2 spectre:::calculate_solution_commonness_rcpp(new_solution)     1.12m  1.12m   0.0148     34.3MB        0     1     0
+# 3 spectre:::calculate_solution_commonness_rcpp_old(new_solution) 2.54m  2.54m   0.00657    34.3MB        0     1     0
+
+bm2 <- bench::mark(
   spectre:::calculate_solution_commonness_site_rcpp_p(new_solution, solution_commonness, random_col),
-  spectre:::calculate_solution_commonness_site_rcpp(new_solution, solution_commonness, random_col),
-  calculate_solution_commonness_site_R(new_solution, solution_commonness, random_col),
-  iterations = 100
-)
+  spectre:::calculate_solution_commonness_site_rcpp(new_solution, solution_commonness, random_col), iterations = 100)
+
+# 1 spectre:::calculate_solution_commonness_site_rcpp_p(new_solution, solution_commonness, random_col)  2.05ms  3.2ms
+# 2 spectre:::calculate_solution_commonness_site_rcpp(new_solution, solution_commonness, random_col)   46.82ms 46.9ms
+
 */
