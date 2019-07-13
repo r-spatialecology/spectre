dfde161964b376b02c8d05aade077ed27488785d
Sebastian Hanß <sebastian@hanss.info>
2019-07-13 14:14:39 +0200

    Fixes RNG issue and indexing issue

--- src/rcpp_sample.cpp
+++ src/rcpp_sample.cpp
@@ -1,4 +1,5 @@
 #include <Rcpp.h>
+#include "rcpp_sample.h"
 using namespace Rcpp;
 
 //' rcpp_sample
@@ -21,9 +22,9 @@ using namespace Rcpp;
 //' @export
 // [[Rcpp::export]]
 IntegerVector rcpp_sample(IntegerVector x, int size, bool replace = false) {
-  
+
   return sample(x, size, replace);
-  
+
 }
 
 // [[Rcpp::export]]
@@ -32,25 +33,44 @@ IntegerVector which_not(IntegerVector x, int y) {
   return v[x != y];
 }
 
+//// [[Rcpp::export]]
+//IntegerVector get_swap_rows_rcpp(const IntegerVector v) {
+//  RNGScope scope;
+//  IntegerVector retval(2);
+//  int n_rows = v.length();
+//  int species1 = R::runif(1, 140);
+//  for (int i = 0; i < 100; i++) {
+//      std::cout << R::runif(1, 140) << std::endl;
+//  }
+//  IntegerVector xxx = sample(n_rows, 139);
+//  auto x = as<std::vector<int> >(xxx);
+//  auto yy = runif(139, 1, 140);
+//  auto y = as<std::vector<int> >(yy);
+//  int species2 = rcpp_sample(which_not(v, species1), 10)[0];
+//  retval[0] = species1; retval[1] = species2;
+//  assert(v[species1] != v[species2]);
+//  return(retval);
+//}
+
 // [[Rcpp::export]]
 IntegerVector get_swap_rows_rcpp(const IntegerVector v) {
+  RNGScope scope; ///TODO: understand why this is needed
   unsigned species1 = sample(IntegerVector(seq_len(v.length())), 1)[0];
-  unsigned species2 = sample(which_not(v, species1), 1)[0];
-  return(IntegerVector::create(species1, species2));
+  unsigned species2 = sample(which_not(v, v[species1]), 1)[0];
+  return(IntegerVector::create(species1 + 1, species2 + 1)); // R starts counting at 1
 }
 
 // [[Rcpp::export]]
 IntegerVector get_swap_rows_rcpp_bruteforce(const IntegerVector v) {
-  
+  RNGScope scope; ///TODO: understand why this is needed
   IntegerVector rows(seq_len(v.length()));
   unsigned species1 = sample(rows, 1)[0];
   unsigned species2 = sample(rows, 1)[0];
-  
+
   while ((v[species1] + v[species2]) != 1) {
     species2 = sample(rows, 1)[0];
   }
-  
-  return(IntegerVector::create(species1, species2));
+  return(IntegerVector::create(species1 + 1, species2 + 1)); // R starts counting at 1
 }
 
 /*** R
@@ -66,7 +86,6 @@ bench::mark(rcpp_sample(x = 1:10, size = 20, replace = TRUE),
             check = FALSE, relative = TRUE, iterations = 10000)
 
 mat <- matrix(sample(0:1, 480000, replace = TRUE), 6000, 8000)
-site <- 68
 
 x <- spectre:::get_swap_rows(mat[, site])
 mat[x[1], site] != mat[x[2], site]
@@ -75,8 +94,11 @@ mat[x[1], site] != mat[x[2], site]
 x <- spectre:::get_swap_rows_rcpp_bruteforce(mat[, site])
 mat[x[1], site] != mat[x[2], site]
 
-bench::mark(spectre:::get_swap_rows(mat[, site]), 
+bench::mark(spectre:::get_swap_rows(mat[, site]),
             spectre:::get_swap_rows_rcpp(mat[, site]),
             spectre:::get_swap_rows_rcpp_bruteforce(mat[, site]),
-            check = FALSE) 
+            check = FALSE)
+# 1 spectre:::get_swap_rows(mat[, site])                 321µs 652.8µs      909.   134.2KB        0   100     0
+# 2 spectre:::get_swap_rows_rcpp(mat[, site])            162µs 270.1µs     2942.   143.4KB        0   100     0
+# 3 spectre:::get_swap_rows_rcpp_bruteforce(mat[, site])  63µs  86.8µs     9964.    72.9KB        0   100     0
 */
