349fc32120aff3864f6ac5568092b218f8631c3f
Sebastian Hanß <sebastian@hanss.info>
2019-07-13 17:05:16 +0200

    Bugfix. Should finally work

--- src/rcpp_sample.cpp
+++ src/rcpp_sample.cpp
@@ -1,5 +1,4 @@
 #include <Rcpp.h>
-#include "rcpp_sample.h"
 using namespace Rcpp;
 
 //' rcpp_sample
@@ -29,48 +28,31 @@ IntegerVector rcpp_sample(IntegerVector x, int size, bool replace = false) {
 
 // [[Rcpp::export]]
 IntegerVector which_not(IntegerVector x, int y) {
-  IntegerVector v = seq(0, x.size()-1);
+  IntegerVector v = seq(1, x.size());
   return v[x != y];
 }
 
-//// [[Rcpp::export]]
-//IntegerVector get_swap_rows_rcpp(const IntegerVector v) {
-//  RNGScope scope;
-//  IntegerVector retval(2);
-//  int n_rows = v.length();
-//  int species1 = R::runif(1, 140);
-//  for (int i = 0; i < 100; i++) {
-//      std::cout << R::runif(1, 140) << std::endl;
-//  }
-//  IntegerVector xxx = sample(n_rows, 139);
-//  auto x = as<std::vector<int> >(xxx);
-//  auto yy = runif(139, 1, 140);
-//  auto y = as<std::vector<int> >(yy);
-//  int species2 = rcpp_sample(which_not(v, species1), 10)[0];
-//  retval[0] = species1; retval[1] = species2;
-//  assert(v[species1] != v[species2]);
-//  return(retval);
-//}
-
 // [[Rcpp::export]]
 IntegerVector get_swap_rows_rcpp(const IntegerVector v) {
-  RNGScope scope; ///TODO: understand why this is needed
-  unsigned species1 = sample(IntegerVector(seq_len(v.length())), 1)[0];
-  unsigned species2 = sample(which_not(v, v[species1]), 1)[0];
-  return(IntegerVector::create(species1 + 1, species2 + 1)); // R starts counting at 1
+  //RNGScope scope; // needed for debugging
+  unsigned species1 = sample(IntegerVector(seq(1, v.size())), 1)[0];
+  unsigned species2 = sample(which_not(v, v[species1 - 1]), 1)[0];  // R starts counting at 1
+  return(IntegerVector::create(species1, species2));
 }
 
 // [[Rcpp::export]]
 IntegerVector get_swap_rows_rcpp_bruteforce(const IntegerVector v) {
-  RNGScope scope; ///TODO: understand why this is needed
-  IntegerVector rows(seq_len(v.length()));
+  //RNGScope scope; // needed for debugging
+  IntegerVector rows = seq(1, v.size());
   unsigned species1 = sample(rows, 1)[0];
   unsigned species2 = sample(rows, 1)[0];
-
-  while ((v[species1] + v[species2]) != 1) {
+  
+  ///TODO: This will run infinite if the vector contains only zeros or only ones (i.e. cannot swapped)
+  while (v[species1 - 1] == v[species2 - 1]) {  // R starts counting at 1
     species2 = sample(rows, 1)[0];
   }
-  return(IntegerVector::create(species1 + 1, species2 + 1)); // R starts counting at 1
+  
+  return(IntegerVector::create(species1, species2));
 }
 
 /*** R
@@ -80,12 +62,17 @@ rcpp_sample(x = 1:5, size = 10, replace = TRUE)
 bench::mark(rcpp_sample(x = 1:100, size = 5),
             sample(x = 1:100, size = 5),
             check = FALSE, relative = TRUE, iterations = 10000)
-
+# 1 rcpp_sample(x = 1:100, size = 5)  1      2.01      1         1.15      NaN 10000     0      183ms <int … <df[,…
+# 2 sample(x = 1:100, size = 5)       1.56   1         2.95      1         Inf  9999     1       62ms <int … <df[,…
+                                                                                                                                                                                                                              # … with 2 more variables: time <list>, gc <list>
 bench::mark(rcpp_sample(x = 1:10, size = 20, replace = TRUE),
             sample(x = 1:10, size = 20, replace = TRUE),
             check = FALSE, relative = TRUE, iterations = 10000)
+# 1 rcpp_sample(x = 1:10, size = 20, replace = TRUE)  1      1         1            1      NaN 10000     0    152.2ms
+# 2 sample(x = 1:10, size = 20, replace = TRUE)       1.69   1.02      1.54         1      Inf  9999     1     98.7ms
 
 mat <- matrix(sample(0:1, 480000, replace = TRUE), 6000, 8000)
+site <- 68
 
 x <- spectre:::get_swap_rows(mat[, site])
 mat[x[1], site] != mat[x[2], site]
@@ -97,8 +84,15 @@ mat[x[1], site] != mat[x[2], site]
 bench::mark(spectre:::get_swap_rows(mat[, site]),
             spectre:::get_swap_rows_rcpp(mat[, site]),
             spectre:::get_swap_rows_rcpp_bruteforce(mat[, site]),
-            check = FALSE)
-# 1 spectre:::get_swap_rows(mat[, site])                 321µs 652.8µs      909.   134.2KB        0   100     0
-# 2 spectre:::get_swap_rows_rcpp(mat[, site])            162µs 270.1µs     2942.   143.4KB        0   100     0
-# 3 spectre:::get_swap_rows_rcpp_bruteforce(mat[, site])  63µs  86.8µs     9964.    72.9KB        0   100     0
+            check = FALSE, relative = TRUE, iterations = 10000)
+
+# Large, "dense" matrix (i.e. 'mat' from above)
+# 1 spectre:::get_swap_rows(mat[, site])                  2.96   3.45      1         1.84     1.13  9985    15
+# 2 spectre:::get_swap_rows_rcpp(mat[, site])             3.45   3.95      1.11      1.81     1     9988    12
+# 3 spectre:::get_swap_rows_rcpp_bruteforce(mat[, site])  1      1         4.03      1        1.81  9994     6
+
+# small, "sparse" matrix (i.e. 'current_solution' from vignette)
+# 1 spectre:::get_swap_rows(mat[, site])                  1.68   2.16      1         1.67     1.36  9997     3
+# 2 spectre:::get_swap_rows_rcpp(mat[, site])             1.17   1.18      2.20      1.22     1     9999     1
+# 3 spectre:::get_swap_rows_rcpp_bruteforce(mat[, site])  1      1         2.43      1        2.21  9998     2
 */
