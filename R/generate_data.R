8163665fc61bd187b47f9f6ff7c88c754a17a7c4
mspngnbrg <m.spangenberg@posteo.de>
2020-05-10 14:12:12 +0200

    added get_sample_points

--- R/generate_data.R
+++ R/generate_data.R
@@ -19,3 +19,8 @@ generate_data_simple <- function(total_gamma, n_sites, alpha_list) {
   target <- calculate_solution_commonness_rcpp(current_solution)
   return(target)
 }
+
+get_sample_points <- function(successful_iterations, n_sample_points){
+  sample_points <- c(1, seq(round(successful_iterations/n_sample_points), (successful_iterations -1), round(successful_iterations/n_sample_points))                      ,successful_iterations)
+    return(sample_points)
+}
