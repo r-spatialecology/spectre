0190f9a4e5e71a5a4cf84fbe6bf15b637353de00
Sebastian Hanß <sebastian@hanss.info>
2020-06-17 19:10:40 +0200

    Adds `run_optimization_min_conf_0`

---
+++ R/run_optimization_min_conf_0.R
@@ -0,0 +1,40 @@
+#' @title run_optimization_min_conf_0
+#' 
+#' @description xxx
+#' 
+#' @param alpha_list Matrix of predicted alpha diversity in each cell.
+#' @param total_gamma Total (estimated) species in the system.
+#' @param target Pairwise matrix of species in common.
+#' @param fixed_species Partial solution with species that are considered as given.
+#' @param max_runs Max number of loops before stopping.
+#' @param energy_threshold Optimization stops if energy threshold is reached. This is set as a value between 0 and 1 determining the proportion of error accepted
+#' @param seed Seed for random number generator. seed = 0 means that a time stamp is used as seed. 
+#' @param verbose It TRUE, progress report is printed
+#' 
+#' @details 
+#' This is the function which runs the optimization algorithm.
+#' 
+#' @return Best matrix of common species between each site.
+#' @references xxx
+
+#' @export
+run_optimization_min_conf_0 <- function(alpha_list, 
+                                      total_gamma, 
+                                      target,
+                                      fixed_species = matrix(),
+                                      max_runs,
+                                      energy_threshold,
+                                      seed = 0,
+                                      verbose = TRUE) {
+  
+  result = optimizer_min_conf0(alpha_list = alpha_list, 
+                               total_gamma = total_gamma, 
+                               target = target, 
+                               fixed_species,
+                               max_iterations = max_runs,
+                               energy_threshold = energy_threshold,
+                               seed = seed, 
+                               verbose = verbose)
+  
+  return(result)
+}
