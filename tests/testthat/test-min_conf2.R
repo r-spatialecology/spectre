04061ffa3f3ca2c6e65be8bea326e2f04557be9f
Sebastian Hanß <sebastian@hanss.info>
2020-06-18 09:47:43 +0200

    Small changes due to changes in function parameter names

--- tests/testthat/test-min_conf2.R
+++ tests/testthat/test-min_conf2.R
@@ -10,10 +10,11 @@ alpha_list_test <- testdata %>% summarise_all(sum) %>% as.numeric()
 total_gamma_test <- testdata %>% filter_all(any_vars(sum(.) != 0)) %>% nrow()
 target_matrix_test <- testdata %>% as.matrix() %>% spectre:::calculate_solution_commonness_rcpp()
 
-res_sim1 <- spectre:::optimizer_min_conf2(alpha_list_test, 
-                                          total_gamma_test, 
-                                          target_matrix_test,
-                                          matrix() ,200, 0.0,
+res_sim1 <- run_optimization_min_conf_2(alpha_list = alpha_list_test, 
+                                          total_gamma = total_gamma_test, 
+                                          target = target_matrix_test,
+                                          max_iterations = 200, 
+                                          energy_threshold = 0.0,
                                           verbose = FALSE)
 suppressWarnings(
   resultdata <- res_sim1$optimized_grid %>% as_tibble()
--- tests/testthat/test-run_optimization_min_conf.R
+++ tests/testthat/test-run_optimization_min_conf.R
@@ -11,32 +11,32 @@ alpha_list_test <- testdata %>% summarise_all(sum) %>% as.numeric()
 total_gamma_test <- testdata %>% filter_all(any_vars(sum(.) != 0)) %>% nrow()
 target_matrix_test <- testdata %>% as.matrix() %>% spectre:::calculate_solution_commonness_rcpp()
 
-res_sim1 <- spectre::run_optimization_min_conf(alpha_list = alpha_list_test, 
+res_sim1 <- run_optimization_min_conf(alpha_list = alpha_list_test, 
+                                      total_gamma = total_gamma_test, 
+                                      target = target_matrix_test, 
+                                      max_iterations = 200, 
+                                      energy_threshold = 0.0,
+                                      verbose = FALSE)
+res_sim2 <- run_optimization_min_conf_1(alpha_list = alpha_list_test, 
                                         total_gamma = total_gamma_test, 
-                                        target = target_matrix_test, 
-                                        max_runs = 200, 
+                                        target = target_matrix_test,
+                                        max_iterations = 200, 
                                         energy_threshold = 0.0,
                                         verbose = FALSE)
-res_sim2 <- spectre::run_optimization_min_conf_1(alpha_list = alpha_list_test, 
-                                               total_gamma = total_gamma_test, 
-                                               target = target_matrix_test, 
-                                               max_runs = 200, 
-                                               energy_threshold = 0.0,
-                                               verbose = FALSE)
-res_sim3 <- spectre::run_optimization_min_conf_2(alpha_list = alpha_list_test, 
-                                               total_gamma = total_gamma_test, 
-                                               target = target_matrix_test, 
-                                               max_runs = 200, 
-                                               energy_threshold = 0.0,
-                                               verbose = FALSE)
+res_sim3 <- run_optimization_min_conf_2(alpha_list = alpha_list_test, 
+                                        total_gamma = total_gamma_test, 
+                                        target = target_matrix_test, 
+                                        max_iterations = 200, 
+                                        energy_threshold = 0.0,
+                                        verbose = FALSE)
 suppressWarnings(
   resultdata <- res_sim1$optimized_grid %>% as_tibble()
 )
 suppressWarnings(
-resultdata1 <- res_sim2$optimized_grid %>% as_tibble()
+  resultdata1 <- res_sim2$optimized_grid %>% as_tibble()
 )
 suppressWarnings(
-resultdata2 <- res_sim3$optimized_grid %>% as_tibble()
+  resultdata2 <- res_sim3$optimized_grid %>% as_tibble()
 )
 
 
