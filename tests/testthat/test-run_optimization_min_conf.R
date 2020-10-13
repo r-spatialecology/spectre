8ede1d207f385df619ea4a1c2bb2c61736306e89
Sebastian Hanss <sebastian@hanss.info>
2020-10-13 13:24:27 +0200

    :hammer: Updated unit tests

--- tests/testthat/test-run_optimization_min_conf.R
+++ tests/testthat/test-run_optimization_min_conf.R
@@ -1,4 +1,4 @@
-testthat::context("Test min_conf sum norm")
+testthat::context("Test min_conf")
 
 library(dplyr)
 
@@ -9,15 +9,9 @@ testdata <- tibble("1" = c(0,0,0,1,0,0),
                    "5" = c(1,1,1,1,1,0))
 alpha_list_test <- testdata %>% summarise_all(sum) %>% as.numeric()
 total_gamma_test <- testdata %>% filter_all(any_vars(sum(.) != 0)) %>% nrow()
-target_matrix_test <- testdata %>% as.matrix() %>% spectre:::calculate_solution_commonness_rcpp()
+target_matrix_test <- testdata %>% as.matrix() %>% calculate_solution_commonness_rcpp()
 
 res_sim <- run_optimization_min_conf(alpha_list = alpha_list_test, 
-                                      total_gamma = total_gamma_test, 
-                                      target = target_matrix_test, 
-                                      max_iterations = 200, 
-                                      energy_threshold = 0.0,
-                                      verbose = FALSE)
-res_sim0 <- run_optimization_min_conf_0(alpha_list = alpha_list_test, 
                                         total_gamma = total_gamma_test, 
                                         target = target_matrix_test, 
                                         max_iterations = 200, 
@@ -27,9 +21,6 @@ res_sim0 <- run_optimization_min_conf_0(alpha_list = alpha_list_test,
 suppressWarnings(
   resultdata <- res_sim$optimized_grid %>% as_tibble()
 )
-suppressWarnings(
-  resultdata0 <- res_sim0$optimized_grid %>% as_tibble()
-)
 
 alpha_list_result <- resultdata %>% summarise_all(sum) %>% as.numeric()
 total_gamma_result <- resultdata %>% filter_all(any_vars(sum(.) != 0)) %>% nrow()
@@ -38,11 +29,3 @@ target_matrix_result <- calculate_solution_commonness_rcpp(res_sim$optimized_gri
 testthat::expect_equal(alpha_list_test, alpha_list_result)
 testthat::expect_equal(total_gamma_test, total_gamma_result)
 testthat::expect_equal(target_matrix_test, target_matrix_result)
-
-alpha_list_result0 <- resultdata0 %>% summarise_all(sum) %>% as.numeric()
-total_gamma_result0 <- resultdata0 %>% filter_all(any_vars(sum(.) != 0)) %>% nrow()
-target_matrix_result0 <- calculate_solution_commonness_rcpp(res_sim0$optimized_grid)
-
-testthat::expect_equal(alpha_list_test, alpha_list_result0)
-testthat::expect_equal(total_gamma_test, total_gamma_result0)
-testthat::expect_equal(target_matrix_test, target_matrix_result0)
