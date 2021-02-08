ef4359eadfdc3d233df8c2c613e8e7970caca592
Sebastian Hanß <sebastian@hanss.info>
2021-02-08 14:43:24 +0100

    not relevant

--- spectre_rcpp_dev/spectre_rcpp_dev/main.cpp
+++ spectre_rcpp_dev/spectre_rcpp_dev/main.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 #include <RInside.h>
 #include <Rcpp.h>
-#include "../../src/optimizer.h"
-using namespace Rcpp;
+#include <random>
+#include "../../src/minconf.h"
+//#include "../../src/optimizer.h"
+//using namespace Rcpp;
 
 
 int main()
 {
-    RInside R;
+    std::random_device rd;
+    long long seed = rd();
+    std::vector<unsigned> alpha_list = {2, 1, 2};
+    unsigned gamma = 3;
+    std::vector<int> target = {  -10, 0, 2 ,
+                                 0, -10, 0 ,
+                                 2, 0, -10  };
+    MinConf mc(alpha_list, gamma, target, seed);
+
+    // now let's benchmark:
+    //    BENCHMARK("MinConf") {
+    mc.optimize(5000, false, false); // will be finished after <<5k steps
+    //    };
+
+
+   /* RInside R;
     R.parseEvalQ("alpha15 <- readRDS(\"~/sebastian@hanss.info/2_ECOMOD/1_Projects/spectre2/spectre/data/alpha15.rds\")");
     IntegerVector alpha_list = R["alpha15"];
     R.parseEvalQ("load(\"/home/bacchus/sebastian@hanss.info/2_ECOMOD/1_Projects/spectre2/spectre/data/estimated_gamma.rda\")");
@@ -18,20 +35,21 @@ int main()
     R.parseEvalQ("load(\"~/sebastian@hanss.info/2_ECOMOD/1_Projects/spectre2/spectre/data/random_solution.rda\")");
     auto res = optimizer_min_conf(alpha_list, total_gamma, target, IntegerMatrix(), IntegerMatrix(),
                                   50, 0, false, false);
-//    IntegerMatrix sol = R.parseEval("current_solution");
-//    IntegerVector alpha_list = R.parseEval("alpha_list");
-//    int total_gamma = R.parseEval("estimated_gamma");
-//    IntegerMatrix target_matrix = R.parseEval("target_matrix");
+    /*/
+    //    IntegerMatrix sol = R.parseEval("current_solution");
+    //    IntegerVector alpha_list = R.parseEval("alpha_list");
+    //    int total_gamma = R.parseEval("estimated_gamma");
+    //    IntegerMatrix target_matrix = R.parseEval("target_matrix");
 
-//    auto sol_com = calculate_solution_commonness_rcpp(sol);
-//    //R["result"]
-//    auto result = mh_optimizer(alpha_list, total_gamma, target_matrix);
-//    R.parseEvalQ("saveRDS(result, \"result.rds\"");
-////    IntegerMatrix solution_matrix = R.parseEval("matrix(sample(0:1, 48, replace = TRUE), 6, 8)");
-////    IntegerMatrix solution_commonness = R.parseEval("matrix(sample(0:8, 64, replace = TRUE), 8, 8)");
-////    int site = 6;
-//    //IntegerMatrix solution = calculate_solution_commonness_site_rcpp(
-//    //            solution_matrix, solution_commonness, site);
+    //    auto sol_com = calculate_solution_commonness_rcpp(sol);
+    //    //R["result"]
+    //    auto result = mh_optimizer(alpha_list, total_gamma, target_matrix);
+    //    R.parseEvalQ("saveRDS(result, \"result.rds\"");
+    ////    IntegerMatrix solution_matrix = R.parseEval("matrix(sample(0:1, 48, replace = TRUE), 6, 8)");
+    ////    IntegerMatrix solution_commonness = R.parseEval("matrix(sample(0:8, 64, replace = TRUE), 8, 8)");
+    ////    int site = 6;
+    //    //IntegerMatrix solution = calculate_solution_commonness_site_rcpp(
+    //    //            solution_matrix, solution_commonness, site);
 
 
     return 0;
