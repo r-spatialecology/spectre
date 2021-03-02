46791a27a93da1f5b5ca6a911da19d134bc277fe
mspngnbrg <m.spangenberg@posteo.de>
2021-03-02 19:43:07 +0100

    script version for bc test

--- test_bc_MSP/test_bc_MSP.R
+++ test_bc_MSP/test_bc_MSP.R
@@ -111,7 +111,7 @@ sim_fun <- function(siminputrow, parameters, writeRDS, verbose)
                         RCE_sol_obs = RCE_sol_obs,
                         MAE_sol_pred = MAE_sol_pred, 
                         RCE_sol_pred = RCE_sol_pred)
-  saveRDS(r_1, file = paste0("./test_bc_MSP/res_bc_test/comm", siminputrow, ".rds")) # change this later! 
+  saveRDS(r_1, file = paste0("./test_bc_MSP/res_bc/comm", siminputrow, ".rds")) # change this later! 
 }
 
 foreach(REPLICATE = 1:dim(p)[1], .export = c("p"), .packages = c("spectre")) %do% {
