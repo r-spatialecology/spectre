1485617d134a0c985fc2ff8e3b0dd75b241df37a
Matthias Spangenberg <m.spangenberg@posteo.de>
2021-03-03 15:07:21 +0100

    updated results

--- test_bc_MSP/test_bc_MSP_plots.R
+++ test_bc_MSP/test_bc_MSP_plots.R
@@ -5,27 +5,6 @@ library(tidyverse)
 
 ### spectre dev 
 
-df<- list.files(path = paste0(getwd(), "/test_bc_MSP/res"), pattern = "*.rds", full.names = TRUE) %>%
-  map(readRDS) %>%
-  bind_rows()
-
-df$replicate <- as.factor(df$replicate)
-
-ggplot(data = df) + 
-  geom_boxplot(aes(y = RCE_sol_pred))+ 
-  #  geom_hline(yintercept = df$RCE_target_observed[1], 
-  #            linetype = "dotted", 
-  #           size = 1.6, 
-  #          color = "blue" ) + 
-  scale_y_continuous(name = expression(RCE) ) + #, breaks = seq(3.9, 4.0, 0.02 ), limits = c(3.91, 3.98)) + 
-  scale_x_discrete(name ="" )+
-  theme_bw(base_size = 18)
-
-
-ggsave(paste0(getwd(), "/test_bc_MSP/dev_.png"), width = 8, height = 6, units = "in")
-
-### Bray curtis 
-
 df<- list.files(path = paste0(getwd(), "/test_bc_MSP/res_bc"), pattern = "*.rds", full.names = TRUE) %>%
   map(readRDS) %>%
   bind_rows()
@@ -33,7 +12,7 @@ df<- list.files(path = paste0(getwd(), "/test_bc_MSP/res_bc"), pattern = "*.rds"
 df$replicate <- as.factor(df$replicate)
 
 ggplot(data = df) + 
-  geom_boxplot(aes(y = RCE_sol_pred))+ 
+  geom_boxplot(aes(y = RCE_target_observed))+ 
   #  geom_hline(yintercept = df$RCE_target_observed[1], 
   #            linetype = "dotted", 
   #           size = 1.6, 
@@ -42,7 +21,47 @@ ggplot(data = df) +
   scale_x_discrete(name ="" )+
   theme_bw(base_size = 18)
 
+ggplot(data = df) + 
+  geom_boxplot(aes(y = bray_error))+ 
+  #  geom_hline(yintercept = df$RCE_target_observed[1], 
+  #            linetype = "dotted", 
+  #           size = 1.6, 
+  #          color = "blue" ) + 
+  scale_y_continuous(name = expression(BC_error) ) + #, breaks = seq(3.9, 4.0, 0.02 ), limits = c(3.91, 3.98)) + 
+  scale_x_discrete(name ="" )+
+  theme_bw(base_size = 18)
 
-ggsave(paste0(getwd(), "/test_bc_MSP/bc_.png"), width = 8, height = 6, units = "in")
+
+ggsave(paste0(getwd(), "/test_bc_MSP/bc.png"), width = 8, height = 6, units = "in")
+
+### Bray curtis 
+
+df<- list.files(path = paste0(getwd(), "/test_bc_MSP/res_bc_test"), pattern = "*.rds", full.names = TRUE) %>%
+  map(readRDS) %>%
+  bind_rows()
+
+df$replicate <- as.factor(df$replicate)
+
+ggplot(data = df) + 
+  geom_boxplot(aes(y = RCE_target_observed))+ 
+  #  geom_hline(yintercept = df$RCE_target_observed[1], 
+  #            linetype = "dotted", 
+  #           size = 1.6, 
+  #          color = "blue" ) + 
+  scale_y_continuous(name = expression(RCE) ) + #, breaks = seq(3.9, 4.0, 0.02 ), limits = c(3.91, 3.98)) + 
+  scale_x_discrete(name ="" )+
+  theme_bw(base_size = 18)
+
+ggplot(data = df) + 
+  geom_boxplot(aes(y = bray_error))+ 
+  #  geom_hline(yintercept = df$RCE_target_observed[1], 
+  #            linetype = "dotted", 
+  #           size = 1.6, 
+  #          color = "blue" ) + 
+  scale_y_continuous(name = expression(BC_error) ) + #, breaks = seq(3.9, 4.0, 0.02 ), limits = c(3.91, 3.98)) + 
+  scale_x_discrete(name ="" )+
+  theme_bw(base_size = 18)
+
+ggsave(paste0(getwd(), "/test_bc_MSP/bc_test.png"), width = 8, height = 6, units = "in")
 
 
