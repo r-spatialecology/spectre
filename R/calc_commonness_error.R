9254c1c8fa59d8bf4814fd83b97b3052b0795165
mhesselbarth <mhk.hesselbarth@gmail.com>
2021-04-29 07:25:00 -0400

    :sparkles: Wrap error calculation into function

---
+++ R/calc_commonness_error.R
@@ -0,0 +1,27 @@
+#' @title calc_commonness_error
+#' 
+#' @description Calcualte commonness error
+#' 
+#' @param x Results object from run_optimization_min_conf.
+#' @param objective_matrix Matrix from (modelled) alpha-diversity and Bray-Curtis dissimilarity
+#' 
+#' @details 
+#' Calculate mean absolute commonness error (MAE_c) and and relative commonness error [%] (RCE).
+#' 
+#' @return vector
+#' 
+#' @export
+calc_commonness_error <- function(x, objective_matrix) {
+  
+  solution_matrix <- calculate_solution_commonness_rcpp(x$optimized_grid)
+  
+  # mean absolute commonness error
+  MAE_c <- mean(abs(solution_matrix - objective_matrix), na.rm = TRUE) 
+  
+  # relative commonness error [%]
+  RCE <- MAE_c / mean(abs(objective_matrix), na.rm = TRUE) * 100 
+  
+  result <- c(MAE_c = MAE_c, RCE = RCE)
+  
+  return(result)
+}
