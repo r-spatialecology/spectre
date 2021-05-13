e0485fc37294ed6dd3097cc4ee3909f9c5054292
Sebastian Hanß <sebastian@hanss.info>
2021-05-13 11:31:34 +0200

    dev -> main (#97)
    * BUGFIX upper triangle instead of lower triangle in generate_commonness_matrix_from_gdm.R
    
    * add WIP test for generate_commonness_matrix_from_gdm()
    
    * added test, completed
    
    * updated test, tidied
    
    * added more comments
    
    * added gdm as suggested package
    
    * updated DESCRIPTION
    
    * correction gdm back to Suggests
    
    * 🗑 Tidied up data folder - only essientials left
    
    * plot_energy -> plot_error
    
    * :recycle: plot_commonness works with the result of the optimization directly, now
    
    * :recycle: plot_enery -> plot_error
    
    * Changed the remaining "energy" labels to "error" including in global variables (#85)
    
    Co-authored-by: Craig Simpkins <simpkinscraig063@gmail.com>
    
    * deleted defunct functions, related to #75
    
    * updated NAMESPACE and man/ files, related to #75
    
    * Created a simple test for generate_data_raster_rescale
    
    * :white_check_mark: Make sure tests run
    
    * :pencil: cran-comments
    
    * :white_check_mark: Install NLMR from GitHub
    
    * Removed virtualspecies files as this is not used in the package itself
    
    * Updated cran-comments to reflect working on windows.
    
    * Updated authors and maintainers fields
    
    * Update DESCRIPTION
    
    * Updated dependencies
    
    * Updated NEWS.md to say we released the package. We can live in hope
    
    * :pencil: Small changes to NEWS.md which allows us to adapt possible changes asked by CRAN team in v1.1
    
    * :pencil: Update DESCRIPTION to v1.0 as in NEWS
    
    * Adding empty dummy vignette
    
    * add minimal example for the package
    
    * Adressed issues #90 and #89
    
    * :sparkles: Wrap error calculation into function
    
    * Update Vignette get_started
    
    * typo correction
    
    * [skip-ci] :white_check_mark: :pencil: Fixing README workflow...maybe
    
    * [skip-ci] Trigger README build
    
    * Added commented out install instructions to vignette and removed excess suggests from DESCRIPTION
    
    * Created new vignette #91. Please check and add names to authors list
    
    * Updated readme a little and added hex logo :)
    
    * Re-build README.Rmd
    
    Co-authored-by: mspngnbrg <m.spangenberg@posteo.de>
    Co-authored-by: Craig Simpkins <simpkinscraig063@gmail.com>
    Co-authored-by: mhesselbarth <mhk.hesselbarth@gmail.com>
    Co-authored-by: GitHub Actions <actions@github.com>

--- tests/testthat/test-run_optimization_min_conf.R
+++ tests/testthat/test-run_optimization_min_conf.R
@@ -13,10 +13,10 @@ total_gamma_test <- testdata %>% filter_all(any_vars(sum(.) != 0)) %>% nrow()
 target_matrix_test <- testdata %>% as.matrix() %>% spectre:::calculate_solution_commonness_rcpp()
 
 res_sim <- run_optimization_min_conf(alpha_list = alpha_list_test, 
-                                        total_gamma = total_gamma_test, 
-                                        target = target_matrix_test, 
-                                        max_iterations = 200, 
-                                        verbose = FALSE)
+                                     total_gamma = total_gamma_test, 
+                                     target = target_matrix_test, 
+                                     max_iterations = 200, 
+                                     verbose = FALSE)
 
 resultdata <- res_sim$optimized_grid %>% as.data.frame()
 
@@ -28,4 +28,3 @@ expect_equal(alpha_list_test, alpha_list_result)
 expect_equal(total_gamma_test, total_gamma_result)
 expect_equal(target_matrix_test, target_matrix_result)
 expect_equal(min(res_sim$error$error), 0)
-
