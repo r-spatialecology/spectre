e79d19d36b0153f3e8e22ad467a1f92c64f75af6
Matthias_P24 <m.spangenberg@posteo.de>
2021-07-07 10:23:45 +0200

    remove gdm dependencies from tests and examples

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,5 @@
 library(testthat)
 library(spectre)
-
 library(dplyr)
-library(gdm)
 
 test_check("spectre")
---
+++ tests/testthat/test-generate_commonness_matrix_from_gdm.R
@@ -0,0 +1,21 @@
+# Tests  
+# 1. that generate_commonness_matrix_from_gdm() correctly calculates a commonness matrix given siteXsite values for 
+# Bray-Curtis dissimilarity and per site richness
+# 2. that siteXsite order used in the gdm package is the same as assumed by generate_commonness_matrix_from_gdm(). 
+# Since siteXsite order in upcoming versions of the gdm package could change, we test whether orders match. 
+
+# We created a random species composition (15 sites, gamma diversity = 20) and calculated $\alpha$-diversity and 
+# Bray-Curtis dissimilarity to be used as input for the `spectre` algorithm 
+# (please see `R/generate_minimal_example_data.R` for details). 
+
+# load random species composition data
+d <- readRDS("./data/minimal_example_data.rds")
+
+# calculate commonness matrix directly from the random species list
+obj_commonness <- spectre:::calculate_solution_commonness_rcpp( d$species_list )
+
+# Re-calculate commonness from observed siteXsite Bray-Curtis dissimilarity values and per site richness 
+rec_commonness <- spectre:::generate_commonness_matrix_from_gdm(gdm_predictions = d$beta_list, 
+                                                                alpha_list = d$alpha_list)
+expect_equal(obj_commonness, rec_commonness)
+
